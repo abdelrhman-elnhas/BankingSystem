62bdb395de89dfffeb9fe11b290bb09bc2e42805
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2023-02-08 15:32:52 +0200

    Customers Modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Transfer Button Animation
 const button = document.querySelector(".transferBtn");
 
 button.addEventListener("click" , (e) =>{
@@ -8,3 +9,14 @@ button.addEventListener("click" , (e) =>{
     button.classList.remove("animate");
   } ,500);
 });
+
+// Transfer Toast
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", () => {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
